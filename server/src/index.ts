19bc761bbd639eb6b17049d8d9488b3dbb6d2cb1
Modi Meet <meetmodi8080@gmail.com>
2026-01-09 11:15:46 +0530

    feat: setup backend seerver + connect database

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import { connectDB } from './config/db';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Database Connection
+connectDB();
+
+// Routes
+import authRoutes from './routes/authRoutes';
+import taskRoutes from './routes/taskRoutes';
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('PrimeTrade API is running');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
