42023d71373bdc5b5136a353cb328481e00fb905
Modi Meet <meetmodi8080@gmail.com>
2026-01-09 11:16:19 +0530

    feat: Add Task and User schema models

---
+++ server/src/models/User.ts
@@ -0,0 +1,51 @@
+// @ts-nocheck
+import mongoose, { Document, Model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    matchPassword(enteredPassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+    // @ts-ignore
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    // @ts-ignore
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const User: Model<IUser> = mongoose.model<IUser>('User', userSchema);
+
+export default User;
